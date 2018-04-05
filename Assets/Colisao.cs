3a45d0582823e1de04ba641b758e7240a665bc42
Miragem Team <owlknightprin@gmail.com>
2018-04-05 17:05:18 -0300

    Primeiro commit do projeto

---
+++ Assets/Colisao.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Colisao : MonoBehaviour {
+    float minY, maxY, minX, maxX;
+    Player player;
+	// Use this for initialization
+	void Start () {
+        player = FindObjectOfType<Player>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    	
+	}
+
+    private bool Colide()
+    {
+        if
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public float minY, maxY, minX, maxX;
+    float gravity = 0.4f;
+    Vector2 speed;
+    public bool onFloor; 
+
+	// Use this for initialization
+	void Start () {
+         
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        minX = GetComponent<SpriteRenderer>().sprite.bounds.min.x;
+        maxY = GetComponent<SpriteRenderer>().sprite.bounds.max.y;
+        maxX = GetComponent<SpriteRenderer>().sprite.bounds.max.x;
+        minY = GetComponent<SpriteRenderer>().sprite.bounds.min.y;
+
+        if (!onFloor)
+        {
+            speed.y -= gravity;
+        }
+
+        transform.position = new Vector3(Input.GetAxisRaw("Horizontal") * speed.x, speed.y);
+		
+
+
+	}
+}
